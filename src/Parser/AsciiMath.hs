ab239fb3538e6e9f5a4d7def3c7e783979c1ff9a
Conflagrationator <willmarrujo@gmail.com>
2017-05-19 16:36:42 -0500

    AsciiMath Translation Attepmt

--- src/Parser/AsciiMath.hs
+++ src/Parser/AsciiMath.hs
@@ -1,6 +1,10 @@
 -- PARSING AsciiMath
 --------------------------------------------------------------------------------
 
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Parser.AsciiMath where
 
 --------------------------------------------------------------------------------
@@ -46,8 +50,8 @@ data Expr
 instance Show Expr where
     show (Const s) = s
     show (Group s a) = "[\"" ++ s ++ "\": " ++ show a ++ "]"
-    show (Unary s a) = "(" ++ s ++ ":" ++ show a ++ ")"
-    show (Binary s a b) = "(" ++ s ++ ": " ++ show a ++ " " ++ show b ++ ")"
+    show (Unary name a) = "(" ++ name ++ ":" ++ show a ++ ")"
+    show (Binary name a b) = "(" ++ name ++ ": " ++ show a ++ " " ++ show b ++ ")"
     show (Func name sub sup args) = "(" ++ name ++ ": " ++ (if isJust sub then "_(" ++ show (fromJust sub) ++ ") " else "") ++ (if isJust sup then "^(" ++ show (fromJust sup) ++ ") " else "") ++ "(" ++ show args ++ "))"
 
 --------------------------------------------------------------------------------
@@ -76,7 +80,7 @@ parseNumber = do
 
 parseGreek :: Parser String
 parseGreek = choice (map string greekLetters) <?> "greek letter"
-    
+
 parseIdent :: Parser String -- only single letter identifiers for now
 parseIdent = fmap (\x -> [x]) letterChar
 
@@ -105,7 +109,7 @@ parseFunction = do
 
 -- | parses a segment of the expression
 parseTerm :: Parser Expr
-parseTerm = parseGroup <|> parseConst <?> "term"
+parseTerm = try parseFunction <|> parseGroup <|> parseConst <?> "term"
 
 -- | parses operators and segments
 parseExpression :: Parser Expr
@@ -175,12 +179,47 @@ functionNames = [
 --------------------------------------------------------------------------------
 -- translates parsed expression tree to actual expression structure
 
--- translateExpr :: Expression e r => Expr -> Maybe e
--- translateExpr (Const s) = translateNumber s
--- translateExpr (Group s a) = 
--- translateExpr (Unary s a) = 
--- translateExpr (Binary s a b) = 
--- translateExpr (Func name sub sup args) = 
+-- TODO: make something that translates the tree
+-- FIXME: the issue is to make a function that takes an Expr and returns 
+--        Maybe(Expression) of any kind of Expression. the type of the Expression
+--        cannot be known at compile time
+-- TODO: maybe making a wrapper type that encapsulates an expression, then
+--       unwrapping it and potentially failing when putting it in a container
+--       expression, then finally returning on the outermost layer a wrapper,
+--       knowing that ther is some valid expression inside, just not knowing the type of it.
+
+-- NOTE: 1st attempt
+
+--------------------------------------------------------------------------------
+-- WRAPPER TYPE
+
+data ExpressionWrapper = forall e r. Expression e r => WrappedExpression e
+
+instance Show ExpressionWrapper where
+    show (WrappedExpression e) = show e
+
+wrapExpression :: Expression e r => e -> ExpressionWrapper
+wrapExpression = WrappedExpression
+
+--------------------------------------------------------------------------------
+-- TRANSLATOR
+
+translateExpression :: Expr -> Maybe ExpressionWrapper
+translateExpression (Const s)
+    | isJust (translateNumber s) = fmap wrapExpression (translateNumber s)
+    | isJust (translateGreek s) = fmap wrapExpression (translateGreek s)
+    | isJust (translateIdent s) = fmap wrapExpression (translateIdent s)
+    | otherwise = Nothing
+-- translateExpression (Group s a)
+-- translateExpression (Unary name a)
+-- translateExpression (Binary name a b)
+--     | name == "+" = fmap wrapExpression (translateAddition (translateExpression a) (translateExpression b))
+--     -- TODO: find out where to put checking for name string equality (for use with more complicated ones like log and ln, etc.)
+--     | otherwise = Nothing
+-- translateExpression (Func name sub sup args)
+
+--------------------------------------------------------------------------------
+-- SPECIFIC TRANSLATORS
 
 translateNumber :: String -> Maybe Number
 translateNumber s = if isJust (readMaybe s :: Maybe Double) then Just (Measure (fromJust (readMaybe s :: Maybe Double)) unitless) else Nothing -- FIXME: find out a way to read units
@@ -191,12 +230,21 @@ translateGreek s = if s `elem` greekLetters then Just (Variable s) else Nothing
 translateIdent :: String -> Maybe Variable
 translateIdent s = if length s == 1 then Just (Variable s) else Nothing -- TODO: add support for checking if they're defined already. or should that go in the parser?
 
+-- translateAddition :: (Expression a n, Expression b m, Addable n m c) => String -> Maybe ExpressionWrapper -> Maybe ExpressionWrapper -> Maybe (Addition a b c)
+-- translateAddition s e1 e2 -- if e1 is type a & e2 is type b where they are also addable to c
+--     | s == "+" && isJust e1 && isJust e1 = undefined
+--     | otherwise = Nothing
 
-
-
-
-    
 --------------------------------------------------------------------------------
 -- WRITER
 --------------------------------------------------------------------------------
 -- takes an actual expression tree and outputs AsciiMath
+
+class AsciiMathDisplayable e where
+    displayAsciiMath :: e -> String
+
+instance AsciiMathDisplayable Number where
+    displayAsciiMath (Measure a u) = show a ++ if u == unitless then "" else "text(" ++ show u ++ ")"
+    -- FIXME: this doesn't actually deal with the real problem
+    -- TODO: need to make it extendable so anyone can write a new writing method for all
+    --       expression instances
